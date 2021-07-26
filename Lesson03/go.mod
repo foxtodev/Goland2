fd1a9031e1573695579b892f4ad998dee29d5891
foxtodev <ka.des@mail.ru>
2021-07-27 03:01:39 +1000

    Lesson03

---
+++ Lesson03/go.mod
@@ -0,0 +1,5 @@
+module github.com/foxtodev/print
+
+go 1.16
+
+require github.com/foxtodev/mymodule v1.0.0
---
+++ Lesson03/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/foxtodev/mymodule"
+)
+
+func main() {
+	fmt.Println(mymodule.Hi("roberto"))
+}
