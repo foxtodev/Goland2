1022f08b077cad679af50717612e2362fd439e5c
foxtodev <ka.des@mail.ru>
2021-07-29 20:14:35 +1000

    Lesson03

--- Lesson03/go.mod
+++ Lesson03/go.mod
@@ -1,4 +1,4 @@
-module github.com/foxtodev/print
+module Lesson03
 
 go 1.16
 
--- Lesson03/main.go
+++ Lesson03/main.go
@@ -8,4 +8,5 @@ import (
 
 func main() {
 	fmt.Println(mymodule.Hi("roberto"))
+	fmt.Println(mymodule.Hi("roberto"))
 }
